5d1ef56b0d1ae0cb699d4b3d1bdfb353b0b113ed
PythonCodeNemesis <durgaminakshmi@gmail.com>
2023-01-28 13:50:05 +0530

    updates to code!

--- SQLAlchemy_dataclass.py
+++ SQLAlchemy_dataclass.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
 Base = declarative_base()
+
 @dataclass
 class Person(Base):
     __tablename__ = 'person'
@@ -12,10 +15,22 @@ class Person(Base):
 from sqlalchemy import create_engine
 engine = create_engine('sqlite:///test.db')
 Base.metadata.create_all(bind=engine)
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
 
 p = Person(name="John", age=30)
 session.add(p)
 session.commit()
 
 all_persons = session.query(Person).all()
-print(all_persons)
+for user in all_persons:
+    print(user.name, user.age)
+
+p = Person(name="John2222", age=3000)
+session.add(p)
+session.commit()
+
+all_persons = session.query(Person).all()
+for user in all_persons:
+    print(user.name, user.age)
